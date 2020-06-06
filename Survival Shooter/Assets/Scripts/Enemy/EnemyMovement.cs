ffb35903c0c0c1e746d9a61123a9a36509a99aae
Matejas S <matejas.santl@student.mev.hr>
2020-06-06 22:57:20 +0200

    gotovo

---
+++ Survival Shooter/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform igrac;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    UnityEngine.AI.NavMeshAgent nav;
+
+
+    void Awake ()
+    {
+		igrac = GameObject.FindGameObjectWithTag ("Player").transform;
+		playerHealth = igrac.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent <EnemyHealth> ();
+        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+    }
+
+
+    void Update ()
+    {
+        if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
+		nav.SetDestination (igrac.position);
+        }
+        else
+        {
+           nav.enabled = false;
+        }
+    }
+}
